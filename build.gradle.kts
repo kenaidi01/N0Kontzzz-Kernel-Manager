469cfb78e4d6578303b679753bd888a21b7fdee3
Muhammad Bimo Habib Al-Farrabi <bimoalfarrabi24@gmail.com>
2025-10-04 10:55:52 +0700

    chore(hilt): version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("com.google.dagger.hilt.android") version "2.57.1" apply false
+    id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20" apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
