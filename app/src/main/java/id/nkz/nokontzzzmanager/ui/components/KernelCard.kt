55e4a88a5852ac7cc06f3954fdf72a1256bc3a6d
Muhammad Bimo Habib Al-Farrabi <bimoalfarrabi24@gmail.com>
2025-10-15 11:06:46 +0700

    chore(kernel): Clean up unused import

--- app/src/main/java/id/nkz/nokontzzzmanager/ui/components/KernelCard.kt
+++ app/src/main/java/id/nkz/nokontzzzmanager/ui/components/KernelCard.kt
@@ -3,10 +3,8 @@ package id.nkz.nokontzzzmanager.ui.components
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Security
 import androidx.compose.material.icons.filled.Memory
 import androidx.compose.material.icons.filled.Computer
 import androidx.compose.material.icons.filled.Shield
@@ -21,11 +19,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.compose.foundation.clickable
 import androidx.compose.ui.draw.clip
 import id.nkz.nokontzzzmanager.data.model.KernelDetailInfo
