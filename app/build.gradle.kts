344577d1f46246857ca66934c8c27ec0a2142dd9
Muhammad Bimo Habib Al-Farrabi <bimoalfarrabi24@gmail.com>
2025-10-15 11:59:21 +0700

    NKM: Release 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "id.nkz.nokontzzzmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "1.0.4"
+        versionCode = 27
+        versionName = "1.0.5"
     }
     buildTypes {
         release {
