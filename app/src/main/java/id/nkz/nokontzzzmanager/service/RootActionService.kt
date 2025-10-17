6ad7c6207e985a32c22c2cd1a50b761ee6b2edc1
Muhammad Bimo Habib Al-Farrabi <bimoalfarrabi24@gmail.com>
2025-10-17 09:04:24 +0700

    chore(tiles): remove unused import

--- app/src/main/java/id/nkz/nokontzzzmanager/service/RootActionService.kt
+++ app/src/main/java/id/nkz/nokontzzzmanager/service/RootActionService.kt
@@ -5,7 +5,6 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
